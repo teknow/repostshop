af0660225cef57b7895d90263ddcd62c5b1cf51d
teknow <thiago@teknow.com.br>
2018-09-17 09:29:48 -0300

    login e logout admin

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php 
 
+session_start();
+
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -19,6 +22,8 @@ $app->get('/', function() {
 });
 
 $app->get('/admin', function() {
+
+	User::verifyLogin();
     
 	$page = new PageAdmin();
 
@@ -26,6 +31,38 @@ $app->get('/admin', function() {
 
 });
 
+$app->get('/admin/login', function() {
+    
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+
+	]);
+
+	$page->setTpl("login");
+
+
+
+});
+
+$app->post('/admin/login', function(){
+
+	User::login($_POST['login'], $_POST['password']);
+
+	header("Location: /admin");
+
+	exit;
+});
+
+$app->get('/admin/logout', function(){
+	
+	User::logout();
+
+	header("Location: /admin/login");
+
+	exit;
+});
+
 $app->run();
 
- ?>
+?>
--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,6 +9,8 @@ class Page {
 	private $tpl;
 	private $options = [];
 	private $defaults = [
+		"header"=>true,
+		"footer"=>true,
 		"data"=>[]
 
 
@@ -19,9 +21,9 @@ class Page {
 		$this->options = array_merge($this->defaults, $opts);
 
 		$config = array(
-			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]. $tpl_dir,
-			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]. "/views-cache",
-			"debug"         => false // set to false to improve the speed
+			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
+			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache",
+			"debug"         => false
 		);
 
 		Tpl::configure($config);
@@ -30,7 +32,7 @@ class Page {
 
 		$this->setData($this->options["data"]);
 
-		$this->tpl->draw("header");
+		if ($this->options["header"] === true) $this->tpl->draw("header");
 
 	}
 
@@ -52,7 +54,7 @@ class Page {
 
 	public function __destruct(){
 
-		$this->tpl->draw("footer");
+		if ($this->options["footer"] === true) $this->tpl->draw("footer");
 
 
 	}
