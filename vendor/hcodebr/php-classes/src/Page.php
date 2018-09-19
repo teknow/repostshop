2963499c8f01928c0bbc66c43eaa1c2fa5b923fe
teknow <thiago@teknow.com.br>
2018-09-18 21:29:33 -0300

    recuperar senha

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -22,7 +22,7 @@ class Page {
 
 		$config = array(
 			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
-			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache",
+			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
 			"debug"         => false
 		);
 
