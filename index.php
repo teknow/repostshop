72dcd324397d8f98db8f6d9e54d825f3d649c6cb
teknow <thiago@teknow.com.br>
2018-09-22 11:38:51 -0300

    Correção no arquivo user

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
+use \Hcode\Model\Category;
 
 $app = new Slim();
 
@@ -241,6 +242,42 @@ $app->post("/admin/forgot/reset", function(){
 
 
 });
+
+$app->get("/admin/categories", function(){
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories", [
+		'categories'=>$categories
+
+	]);
+
+});
+
+$app->get("/admin/categories/create", function(){
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+
+});
+
+$app->post("/admin/categories/create", function(){
+
+	$category = new Category();
+
+	$category->set($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+
+	exit;
+
+});
+
 
 $app->run();
 
