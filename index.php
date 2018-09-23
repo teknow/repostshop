142ed70381c0e22fc65801a6cfe50acf3f7c132f
teknow <thiago@teknow.com.br>
2018-09-23 10:48:57 -0300

    site - categorias

--- index.php
+++ index.php
@@ -335,10 +335,20 @@ $app->post("/admin/categories/:idcategory", function($idcategory){
 
 });
 
+$app->get("/categories/:idcategory", function($idcategory){
 
+	$category = new Category();
 
+	$category->get((int)$idcategory);
 
+	$page = new Page();
 
+	$page->setTpl("category", [
+		'category'=>$category->getValues(),
+		'prodcuts'=>[]
+	]);
+
+});
 
 
 $app->run();
