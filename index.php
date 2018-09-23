139e39a1c5be3bc309c3373b12741821e8df6575
teknow <thiago@teknow.com.br>
2018-09-23 09:54:26 -0300

    CRUD

--- index.php
+++ index.php
@@ -245,6 +245,8 @@ $app->post("/admin/forgot/reset", function(){
 
 $app->get("/admin/categories", function(){
 
+	User::verifyLogin();
+
 	$categories = Category::listAll();
 
 	$page = new PageAdmin();
@@ -258,6 +260,8 @@ $app->get("/admin/categories", function(){
 
 $app->get("/admin/categories/create", function(){
 
+	User::verifyLogin();
+
 	$page = new PageAdmin();
 
 	$page->setTpl("categories-create");
@@ -266,9 +270,11 @@ $app->get("/admin/categories/create", function(){
 
 $app->post("/admin/categories/create", function(){
 
+	User::verifyLogin();
+
 	$category = new Category();
 
-	$category->set($_POST);
+	$category->setData($_POST);
 
 	$category->save();
 
@@ -277,6 +283,62 @@ $app->post("/admin/categories/create", function(){
 	exit;
 
 });
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header('Location: /admin/categories');
+
+	exit;
+
+
+});
+
+$app->get("/admin/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update", [
+		'category'=>$category->getValues()
+	]);
+
+});
+
+$app->post("/admin/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /admin/categories');
+
+	exit;
+
+});
+
+
+
+
+
 
 
 $app->run();
--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -17,6 +17,43 @@ class Category extends Model {
 		return $sql->select("SELECT * FROM tb_categories ORDER BY descategory");
 	}
 
+	public function save()
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_categories_save (:idcategory, :descategory)",
+		array(
+			":idcategory"=> $this->getidcategory(),
+			":descategory"=>$this->getdescategory()
+			));
+
+			$this->setData($results[0]);
+	}
+
+	public function get($idcategory)
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_categories WHERE idcategory = :idcategory", [
+			':idcategory'=>$idcategory
+		]);
+
+		$this->setData($results[0]);
+
+	}
+
+	public function delete()
+	{
+		$sql = new Sql();
+
+		$sql->query("DELETE FROM tb_categories WHERE idcategory = :idcategory", [
+			':idcategory'=>$this->getidcategory()
+		]);
+
+	}
+
 }
 
 ?>
