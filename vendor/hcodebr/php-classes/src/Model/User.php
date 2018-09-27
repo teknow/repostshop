d348df09d5134eb8be1cea10c02979dbf6460ad9
teknow <thiago@teknow.com.br>
2018-09-27 11:08:42 -0300

    Carrinho de Compras

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -13,6 +13,45 @@ class User extends Model {
 
 	const SECRET = "RepostshopSecret";
 
+	public static function getFromSession()
+	{
+		$user = new User();
+
+		if (isset($_SESSION[User::SESSION]) && (int)$_SESSION[User::SESSION]['iduser'] > 0) {
+			$user->setData($_SESSION[User::SESSION]);
+		}
+
+		return $user;
+	}
+
+	public static function checkLogin($inadmin = true)
+	{
+		if(
+			!isset($_SESSION[User::SESSION])
+			||
+			!$_SESSION[User::SESSION]
+			||
+			!(int)$_SESSION[User::SESSION]["iduser"] > 0
+		) {
+			//Não está logado
+			return false;
+		} else {
+
+			if ($inadmin === true && (bool)$_SESSION[User::SESSION]['inadmin'] === true) {
+				return true;
+
+			} else if ($inadmin === false) {
+
+				return true;
+
+			} else {
+
+				return false;
+			}
+		}
+
+	}
+
 	public static function login($login, $password)
 	{
 
@@ -51,16 +90,7 @@ class User extends Model {
 
 	public static function verifyLogin($inadmin = true)
 	{
-		if(
-			!isset($_SESSION[User::SESSION])
-			||
-			!$_SESSION[User::SESSION]
-			||
-			!(int)$_SESSION[User::SESSION]["iduser"] > 0
-			||
-			!(bool)$_SESSION[User::SESSION]["inadim"] !== $inadmin
-
-		){
+		if(User::checkLogin($inadmin)){
 
 			header("Location: /admin/login");
 
